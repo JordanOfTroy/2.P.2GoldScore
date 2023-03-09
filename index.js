93dc14c60cc478b5afa98f2b625dd204626a61d0
Jordan <jordan@Jordans-MacBook-Air.local>
2023-03-09 10:13:57 -0700

    score card being rendered. need to check for player names and apply if logic

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 let courseSelector = document.getElementById('courseSelect')
 let playGolfButton = document.getElementById('playGolf')
-let playersArr = Array.from(document.getElementsByClassName('playerName'))
+// let playersArr = Array.from(document.getElementsByClassName('playerName'))
+let playerInputs = document.querySelector('.playerInputs')
 let addNewPlayerButton = document.getElementById('addNewPlayer')
+let scoreCard = document.getElementById('scoreCard')
 let IdCount = 0
 
 class Player {
@@ -35,31 +37,102 @@ function getGolfScore () {
     return JSON.parse(localStorage.getItem('golfScore'))
 }
 
+function createTableData(parentEle, value) {
+    let tableData = document.createElement('td')
+    tableData.innerText = value
+    parentEle.appendChild(tableData)
+}
+
+function addScoreBoxes (parentEle, scoresArr, len) {
+    for (let i = 0; i < len; i++) {
+        createTableData(parentEle, scoresArr[i] ? scoresArr[i] : '')
+    }
+}
+
+function showScoreCard (parentEle) {
+    let currentGame = getGolfScore()[0]
+    let table = document.createElement('table')
+    let holes = document.createElement('tr')
+    let yards = document.createElement('tr')
+    let par = document.createElement('tr')
+    let holesHeader = document.createElement('th')
+    let yardsHeader = document.createElement('th')
+    let parHeader = document.createElement('th')
+
+    holesHeader.innerText = 'Holes'
+    yardsHeader.innerText = 'Yards'
+    parHeader.innerText = 'Par'
+
+    holes.appendChild(holesHeader)
+    yards.appendChild(yardsHeader)
+    par.appendChild(parHeader)
+
+
+    currentGame.course.holes.forEach((hole) => {
+        createTableData(holes, hole.hole)
+        createTableData(yards, hole.changeLocations[0].yards)
+        createTableData(par, hole.changeLocations[0].par)
+    })
+
+    table.appendChild(holes)
+    table.appendChild(yards)
+    table.appendChild(par)
+
+    currentGame.players.forEach((player) => {
+        let {name, scores} = player
+        let playerRow = document.createElement('tr')
+        let playerHeader = document.createElement('th')
+        playerHeader.innerText = name
+        playerRow.appendChild(playerHeader)
+
+        addScoreBoxes(playerRow, scores, currentGame.course.holes.length)
+
+        table.appendChild(playerRow)
+    })
+
+
+    parentEle.appendChild(table)
+}
+
 function updateLocalStorage(data, arr) {
     checkForGolfScore() ?? setInitialLocalStorage()
 
-   let players = []
-   arr.forEach(player => players.push(new Player(player)))
-   let game = new Game(data, players)
-   let golfScores = getGolfScore()
-   golfScores.push(game)
+    let players = []
+    let game = new Game(data, players)
+    let golfScores = []
+    
+    arr.forEach(player => players.push(new Player(player)))
+    golfScores.push(game)
+
     localStorage.setItem('golfScore', JSON.stringify(golfScores))
+    showScoreCard(scoreCard)
 }
 
 async function getCourse(obj) {
-    (await fetch(`http://uxcobra.com/golfapi/course${obj.course}.txt`)).json().then((res, rej) => {
-      updateLocalStorage(res.data, obj.players)
-    })
-  
+    const response = await fetch(`http://uxcobra.com/golfapi/course${obj.course}.txt`)
+    // console.log(response)
+    const jsonResponse = await response.json()
+    // console.log(jsonResponse)
+    updateLocalStorage(jsonResponse.data, obj.players)
 }
 
+
 playGolfButton.addEventListener('click', () => {
+    let playersArr = Array.from(document.getElementsByClassName('playerName'))
     let course = courseSelector.value 
     let players = []
+    // console.log(playersArr)
     playersArr.forEach(player => players.push(player.value))
     getCourse({course, players})
 })
 
+
+addNewPlayerButton.addEventListener('click', () => {
+    let newPlayerInput = document.createElement('input')
+    newPlayerInput.setAttribute('class', 'playerName')
+    newPlayerInput.setAttribute('type', 'text')
+    playerInputs.appendChild(newPlayerInput)
+})
 
 
 // async function get11819() {
